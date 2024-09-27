1d36b97b602625c5fce5adb963a335952aadcfb4
Devan Chauhan <devanchauhan012@gmail.com>
2024-09-27 15:01:28 +0530

    op change

--- frontend/main/AlgoAnims main project/src/components/signup/Signup.jsx
+++ frontend/main/AlgoAnims main project/src/components/signup/Signup.jsx
@@ -105,27 +105,25 @@ function Signup() {
     }
 
 
-<<<<<<< HEAD
-=======
-
-  useEffect(()=>{
-    // main()
-    
-      gsap.from("#background-video", {
-          scale: 1.2, 
-          opacity: 0.5,
-          duration: 2,
-          ease: "power2.out"
-      });
 
     useEffect(() => {
+      // main()
+
+      gsap.from("#background-video", {
+        scale: 1.2,
+        opacity: 0.5,
+        duration: 2,
+        ease: "power2.out"
+      });
+
+      useEffect(() => {
         const backgroundVideo = document.getElementById("background-video");
         const loginBox = document.querySelector(".login-box");
         const signupBox = document.querySelector(".signup-box");
         const algoanimsHeading = document.querySelector(".algoanims-heading");
-    
+
         let tl = gsap.timeline();
-    
+
         // if (backgroundVideo) {
         //   tl.from(
         //     backgroundVideo,
@@ -133,7 +131,7 @@ function Signup() {
         //     { scale: 1, opacity: 1, duration: 2, ease: "power2.out" }
         //   );
         // }
-    
+
         if (loginBox) {
           tl.from(
             loginBox,
@@ -141,7 +139,7 @@ function Signup() {
             { y: 0, opacity: 1, duration: 1.5, delay: 0.5 }
           );
         }
-    
+
         if (signupBox) {
           tl.from(
             signupBox,
@@ -149,7 +147,7 @@ function Signup() {
             { y: 0, opacity: 1, duration: 1.5, delay: 0.8 }
           );
         }
-    
+
         if (algoanimsHeading) {
           tl.from(
             algoanimsHeading,
@@ -157,7 +155,7 @@ function Signup() {
             { y: 0, opacity: 1, duration: 1.5, delay: 0.8 }
           );
         }
-    
+
         // Cleanup on unmount
         return () => {
           tl.kill();
@@ -168,58 +166,56 @@ function Signup() {
 
 
       gsap.from(".signup-box", {
-          y: 50,
-          opacity: 0,
-          duration: 1.5,
-          delay: 0.8
+        y: 50,
+        opacity: 0,
+        duration: 1.5,
+        delay: 0.8
       });
 
       gsap.from(".algoanims-heading", {
-          y: -50,
-          opacity: 0,
-          duration: 1.5,
-          delay:0.8
+        y: -50,
+        opacity: 0,
+        duration: 1.5,
+        delay: 0.8
       });
-  } , [])
- 
+    }, [])
 
 
 
-  function handler(evt){
+
+    function handler(evt) {
       evt.preventDefault();
       let f = 0;
 
-      for(let data of alldata){
-        if(data.emailId == emailid){
-            alert("your account is already exits!");
-            f = 1;
-            break;
+      for (let data of alldata) {
+        if (data.emailId == emailid) {
+          alert("your account is already exits!");
+          f = 1;
+          break;
         }
       }
 
-      if(f == 0){
-        if(password === repassword){
-            navigate("/home");
-        }else{
-            document.querySelector(".pass").classList.remove("hidden");
+      if (f == 0) {
+        if (password === repassword) {
+          navigate("/home");
+        } else {
+          document.querySelector(".pass").classList.remove("hidden");
         }
       }
 
-      
->>>>>>> 1c871d691f71db6403d55501f3988cf0b3f539b9
-  }
 
-  return (
-    <>
-<<<<<<< HEAD
+    }
 
-      <div className="body">
-        <div className="login-container">
-          <div className="video-container">
+    return (
+      <>
 
-            <video className="video" id="background-video" src="/bg.mp4" autoPlay muted loop />
-            {/* <source src="/bg.mp4" type="video/mp4" />
-=======
+        <div className="body">
+          <div className="login-container">
+            <div className="video-container">
+
+              <video className="video" id="background-video" src="/bg.mp4" autoPlay muted loop />
+              {/* <source src="/bg.mp4" type="video/mp4" />
+
    
     <div className="body">
       <div className="login-container">
@@ -227,100 +223,98 @@ function Signup() {
 
             <video className="video" id="background-video" src="/bg.mp4" autoPlay muted loop/>
                 {/* <source src="/bg.mp4" type="video/mp4" />
->>>>>>> 1c871d691f71db6403d55501f3988cf0b3f539b9
+
 
             {/* <video className="video" id="background-video" autoPlay muted loop>
                 <source src="1726757407553429.mp4" type="video/mp4" />
 
                 Your browser does not support the video tag.
             </video> */}
-          </div>
-
-          <header className="algoanims-heading">
-            AlgoAnims
-          </header>
-
-          <div className="form-container">
-            <div className="login-box" >
-              <div className="logo">
-                <img src="/logo.png" alt="Logo" />
-              </div>
-              <form className="login-form" method='post'
-                onSubmit={(evt) => {
-
-                  const formData = {
-                    uname: evt.target[0].value,
-                    email: emailid,
-                    pass: encrypt,
-                    last_char: password[password.length - 1]
-                  }
-
-
-                  fetch("http://localhost:8080/signup", {
-                    method: 'POST',
-                    headers: {
-                      "Content-type": "application/json"
-                    },
-                    body: JSON.stringify(formData)
-                  }).then((data) => data.json())
-
-
-                  handler(evt);
-                }}
-              >
-                <input type="text" placeholder="username" required />
-                <input
-                  onChange={(evt) => {
-                    setEmailid(evt.target.value);
-                  }}
-                  value={emailid}
-                  type="email" placeholder="Email Address" required />
-                <input type="password"
-
-                  onChange={(evt) => {
-                    setPass(evt.target.value);
-                    setEncrypt(cryptoJs.AES.encrypt(password, getkey()).toString());
-                  }}
-                  value={password}
-                  placeholder="Password" required />
-                <input
-                  onChange={(evt) => {
-                    setRePass(evt.target.value);
-                  }}
-                  value={repassword}
-                  type="password" placeholder="Confirm Password" required />
-                {/* <small className="text-red-600 pass hidden">check your password</small><br /> */}
-                <button type="submit" className="signupbtn">Sign Up</button>
-                <div className="separator">
-                  <div className="line"></div>
-                </div>
-              </form>
             </div>
-            <div className="signup-box">
-<<<<<<< HEAD
-              <p>have an account?
-                <NavLink to='/login' className="text-blue-500">
-                  <span>Log In</span>
-                </NavLink>
-              </p>
-=======
+
+            <header className="algoanims-heading">
+              AlgoAnims
+            </header>
+
+            <div className="form-container">
+              <div className="login-box" >
+                <div className="logo">
+                  <img src="/logo.png" alt="Logo" />
+                </div>
+                <form className="login-form" method='post'
+                  onSubmit={(evt) => {
+
+                    const formData = {
+                      uname: evt.target[0].value,
+                      email: emailid,
+                      pass: encrypt,
+                      last_char: password[password.length - 1]
+                    }
+
+
+                    fetch("http://localhost:8080/signup", {
+                      method: 'POST',
+                      headers: {
+                        "Content-type": "application/json"
+                      },
+                      body: JSON.stringify(formData)
+                    }).then((data) => data.json())
+
+
+                    handler(evt);
+                  }}
+                >
+                  <input type="text" placeholder="username" required />
+                  <input
+                    onChange={(evt) => {
+                      setEmailid(evt.target.value);
+                    }}
+                    value={emailid}
+                    type="email" placeholder="Email Address" required />
+                  <input type="password"
+
+                    onChange={(evt) => {
+                      setPass(evt.target.value);
+                      setEncrypt(cryptoJs.AES.encrypt(password, getkey()).toString());
+                    }}
+                    value={password}
+                    placeholder="Password" required />
+                  <input
+                    onChange={(evt) => {
+                      setRePass(evt.target.value);
+                    }}
+                    value={repassword}
+                    type="password" placeholder="Confirm Password" required />
+                  {/* <small className="text-red-600 pass hidden">check your password</small><br /> */}
+                  <button type="submit" className="signupbtn">Sign Up</button>
+                  <div className="separator">
+                    <div className="line"></div>
+                  </div>
+                </form>
+              </div>
+              <div className="signup-box">
+                <p>have an account?
+                  <NavLink to='/login' className="text-blue-500">
+                    <span>Log In</span>
+                  </NavLink>
+                </p>
+
                 <p>have an account?
 
-                <NavLink to='/login' className="text-blue-500">
-                  <span>Log In</span>
-                </NavLink>
+                  <NavLink to='/login' className="text-blue-500">
+                    <span>Log In</span>
+                  </NavLink>
 
-              
+
 
                 </p>
->>>>>>> 1c871d691f71db6403d55501f3988cf0b3f539b9
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default Signup;
-
