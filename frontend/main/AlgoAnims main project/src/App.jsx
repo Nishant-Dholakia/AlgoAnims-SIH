1f6f19300921fd0c6cce7de2ddfda7b2e1941adc
Nishant Dholakia <nishantdholakia2020@gmail.com>
2024-09-28 15:42:18 +0530

    tried to fix login

--- frontend/main/AlgoAnims main project/src/App.jsx
+++ frontend/main/AlgoAnims main project/src/App.jsx
@@ -3,17 +3,9 @@ import Nav from "./components/navigation/Nav"
 import UseContextProvider from "./contexts/UseContextProvider"
 function App() {
   return (
-<<<<<<< HEAD
       <UseContextProvider>
-  <Nav></Nav>
   <Outlet/>
   </UseContextProvider>
-=======
-    <UseContextProvider>
-    
-      <Outlet/>
-    </UseContextProvider>
->>>>>>> 90e799e5cc0a1f7be6ad41ad1eaa1bd2c2e561f2
   )
 }
 
